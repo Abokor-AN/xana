ce431b4660642a5ee608785bef0ea0f51ea8112e
Abokor-AN <abokor.ahmed.kadar.nour@outlook.com>
2024-02-18 10:24:24 +0300

    Re-initialize project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { ThemeProvider } from "@/components/theme-provider";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,16 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
