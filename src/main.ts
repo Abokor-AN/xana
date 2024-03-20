2f55b83bd84c0351cbb88e3af500f2dad461f24d
Abokor-AN <abokor.ahmed.kadar.nour@outlook.com>
2024-03-20 16:07:13 +0300

    Install Pinia

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ import PrimeVue from "primevue/config";
 // @ts-ignore
 import Lara from "./presets/Lara";
 
+import { createPinia } from "pinia";
+
 const app = createApp(App);
 
 app.use(PrimeVue, {
   unstyled: true,
   pt: Lara,
 });
+
+app.use(createPinia());
 
 app.mount("#app");
